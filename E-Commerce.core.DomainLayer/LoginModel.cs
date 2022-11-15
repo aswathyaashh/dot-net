c97c7574713d82e710f2706bbf06d1a19d31048d
Aswathy Ashok <asashok@suyatitech.local>
2022-11-15 18:46:39 +0530

    login api

--- E-Commerce.core.DomainLayer/LoginModel.cs
+++ E-Commerce.core.DomainLayer/LoginModel.cs
@@ -15,7 +15,7 @@ namespace E_Commerce.core.DomainLayer
         [Required(ErrorMessage = "*Password is required")]
         [Display(Name = "Password")]
         [DataType(DataType.Password)]
-        [RegularExpression("[^ ]{8,16}", ErrorMessage = "Password should contain a minimum of 8 characters and a capital letter")]
+        [RegularExpression("[^ ]{8,10}", ErrorMessage = "Password should contain a minimum of 8 characters and a capital letter")]
         public string password { get; set; }
 
         //[DefaultValue(value:DateTime.Now)]
